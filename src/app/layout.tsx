6e30972b64101f1fc1803e8c1b5b58dca923bb9d
Hernan Alvarado <hernanvid123@gmail.com>
2024-10-05 19:23:30 -0500

    chore: update `useTabs` field in prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,19 +4,19 @@ import { LayoutProps } from "@/lib/@types/props"
 import "@/ui/globals.css"
 
 const gemunu = Gemunu_Libre({
-	subsets: ["latin"],
-	weight: ["300", "400", "500", "700", "800"],
-	variable: "--font-gemunu",
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "700", "800"],
+    variable: "--font-gemunu",
 })
 
 export const metadata: Metadata = {
-	title: "Outer Banks Fans Site",
+    title: "Outer Banks Fans Site",
 }
 
 export default function RootLayout({ children }: LayoutProps) {
-	return (
-		<html lang="en">
-			<body className={gemunu.className}>{children}</body>
-		</html>
-	)
+    return (
+        <html lang="en">
+            <body className={gemunu.className}>{children}</body>
+        </html>
+    )
 }
