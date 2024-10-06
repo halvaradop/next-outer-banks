61d82cc3cca3b8f954778391da26fbab59bc5528
Hernan Alvarado <hernanvid123@gmail.com>
2024-10-05 19:21:23 -0500

    chore: set up `prettier`
    - Added two new commands `format` and `format:check`
    - Ran format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,20 @@ import fire from "@/assets/fire.jpg"
 import island from "@/assets/island.jpg"
 
 const Index = () => {
-
-    return (
-        <main>
-            <section>
-                <div className="text-center uppercase">
-                    <h1 className="fluency-6xl font-bold">Outer Banks</h1>
-                </div>
-                <figure className="w-full max-w-full flex items-center overflow-x-hidden">
-                    <Image className="min-w-full aspect-video object-cover" src={fire} alt="fire picture" />
-                    <Image className="min-w-full aspect-video object-cover" src={van} alt="van picture" />
-                    <Image className="min-w-full aspect-video object-cover" src={island} alt="island picture" />
-                </figure>
-            </section>
-        </main>
-    )
+	return (
+		<main>
+			<section>
+				<div className="text-center uppercase">
+					<h1 className="fluency-6xl font-bold">Outer Banks</h1>
+				</div>
+				<figure className="w-full max-w-full flex items-center overflow-x-hidden">
+					<Image className="min-w-full aspect-video object-cover" src={fire} alt="fire picture" />
+					<Image className="min-w-full aspect-video object-cover" src={van} alt="van picture" />
+					<Image className="min-w-full aspect-video object-cover" src={island} alt="island picture" />
+				</figure>
+			</section>
+		</main>
+	)
 }
 
 export default Index
--- src/lib/@types/props.ts
+++ src/lib/@types/props.ts
@@ -1,4 +1,3 @@
-
 export interface LayoutProps {
-    children: React.ReactNode
+	children: React.ReactNode
 }
